e8bae8192edb1550f2d3ab2d04b4de182e7847b5
Ying <ying@taro.localdomain>
2020-09-19 15:24:01 -0700

    Initial changes for light baking

---
+++ LightBakingExercise/Assets/Plugins/upload.bat
@@ -0,0 +1,1 @@
+curl -v -b cookie -X PUT -F "space_template[zip]=@/Users/ying/altspace/LightBakingExercise/template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1565525078869803757.json
---
+++ README.md
@@ -0,0 +1,3 @@
+
+## Unity light baking exercise
+
